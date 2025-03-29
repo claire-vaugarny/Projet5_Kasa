dbcc51311406ee550629285fcb5d922b86575659
Claire Vaugarny <claire.vaugarny@laposte.net>
2025-03-29 11:36:03 +0100

    ajout de l'animation lors de l'ouverture/fermeture du menu déroulant

--- src/components/Collapse.jsx
+++ src/components/Collapse.jsx
@@ -1,33 +1,46 @@
-import { useState } from 'react';
+import { useState, useRef, useEffect } from 'react';
 import './Collapse.scss';
 import array from '../assets/arrayUp.svg';
 
-
 function Collapse({ collapseTitle, collapseInfo, collapsePage }) {
     const [isOpen, setIsOpen] = useState(false);
+    const contentRef = useRef(null); // Référence au contenu du collapse
+    const [maxHeight, setMaxHeight] = useState('0px'); // Variable d'état pour la hauteur
 
     const toggleCollapse = () => {
         setIsOpen(!isOpen);
     };
+
+    useEffect(() => {
+        if (isOpen) {
+            setMaxHeight(`${contentRef.current.scrollHeight}px`); // Définit la hauteur maximale lors de l'ouverture
+        } else {
+            setMaxHeight('0px'); // Réduit la hauteur à 0 lors de la fermeture
+        }
+    }, [isOpen]); // Réexécute l'effet lorsque isOpen change
 
     return (
         <div className={`collapse-container ${collapsePage || ''}`}>
             <button onClick={toggleCollapse} className="collapse-title">
                 {collapseTitle}
                 <img
-                    src={array} 
+                    src={array}
                     alt="arrow up"
-                    className={`collapse-icon ${isOpen ? 'open' : ''}`} 
+                    className={`collapse-icon ${isOpen ? 'open' : ''}`}
                 />
             </button>
-            {isOpen && (
-                <div className="collapse-content">
+            {isOpen && ( // Le contenu ne sera rendu que lorsque isOpen est true
+                <div
+                    className="collapse-content"
+                    ref={contentRef}
+                    style={{ maxHeight }}
+                >
                     {Array.isArray(collapseInfo) ? (
                         collapseInfo.map((info, index) => (
-                            <p key={index}>{info}</p> // Affiche chaque élément du tableau
+                            <p key={index}>{info}</p>
                         ))
                     ) : (
-                        <p>{collapseInfo}</p> // Si collapseInfo est une seule chaîne
+                        <p>{collapseInfo}</p>
                     )}
                 </div>
             )}
